cb5ac7c0daeb0fa9acc44c154cf860350eb358cb
Kaitlyn VanHook <kaitlynvanhook@Kaitlyns-MacBook-Pro-2.local>
2020-10-06 22:39:19 -0400

    user can delete pins

--- src/javascripts/components/cards/pins.js
+++ src/javascripts/components/cards/pins.js
@@ -1,10 +1,18 @@
+import pinsData from '../../helpers/data/pinsData';
+
 const pinMaker = (pinObject) => {
-  const domString = `<div class="pin-card m-3" id="${pinObject.pinId}" style="width: 18rem;">
+  const domString = `<div class="card pin m-3" id="${pinObject.pinId}" style="width: 18rem;">
                       <a href="${pinObject.url}"><img class="card-img-top" src="${pinObject.imageUrl}" alt=${pinObject.description}"></a>
-                      <div class="card-body">
-                        <p>${pinObject.description}</p>
+                      <div class="card-body d-flex justify-content-between">
+                        <p class="text-left">${pinObject.description}</p>
+                        <i class="fas fa-trash-alt delete-btn" id="${pinObject.pinId}"></i>
                       </div>
                     </div>`;
+  $('body').on('click', '.card.pin .delete-btn', (e) => {
+    e.stopImmediatePropagation();
+    $(`.card#${e.currentTarget.id}`).remove();
+    pinsData.deletePin(e.currentTarget.id);
+  });
   return domString;
 };
 
