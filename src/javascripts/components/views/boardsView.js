2ad694de5ba2faaf1c93f52783e2830a4774d5c6
Kaitlyn VanHook <kaitlynvanhook@Kaitlyns-MacBook-Pro-2.local>
2020-10-08 23:45:06 -0500

    plus sign appears

--- src/javascripts/components/views/boardsView.js
+++ src/javascripts/components/views/boardsView.js
@@ -2,7 +2,7 @@ import boardData from '../../helpers/data/boardsData';
 import boards from '../cards/boards';
 
 const boardsView = () => {
-  $('#app').html('<h1>Boards</h1><div id="boards" class="d-flex justify-content-center"></div>');
+  $('#app').html('<div><h1 class="d-inline-flex">Boards</h1><i class="fa fa-plus d-inline-flex justify-content-end align-self-center mr-4" aria-hidden="true"></i></div><div id="boards" class="d-flex justify-content-center"></div>');
   boardData.getAllBoards()
     .then((response) => {
       if (response.length) {
